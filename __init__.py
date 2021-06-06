b17854ed73ad502eb3fc9d7bf4621642f55ccf9a
jonas-jr <84603271+jonas-jr@users.noreply.github.com>
2021-06-06 10:28:06 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)  #, template_folder='view') #por default use o nome da pasta templates
-
-from app import admin
-from app import cliente
-
-#Este arquivo diz ao interpretador que se trata de um pacote
